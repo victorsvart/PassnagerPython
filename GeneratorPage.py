92f5f7e9455c90e178de901b6669aab67478b523
victorsvart <victor.cscience@gmail.com>
2022-09-10 08:24:00 -0300

    Added Manager Page

--- GeneratorPage.py
+++ GeneratorPage.py
@@ -1,31 +1,64 @@
+from multiprocessing.dummy import Manager
 import tkinter as tk
 from tkinter import *
-from tkinter import ttk
 from PasswordGenerator import *
 
 root = tk.Tk()
 root.title("Passnager")
 root.geometry("1000x600")
-
 root.configure(bg="#00B5C0")
-canvas = Canvas(root,
+
+Generator = tk.Frame(root, width=1000, height=600)
+manager = tk.Frame(root, width=1000, height=600)
+
+
+def showFrame(frame):
+    frame.tkraise()
+
+
+showFrame(Generator)
+
+for frame in (Generator, manager):
+    frame.grid(row=0, column=0, sticky="nsew")
+
+################## GENERATOR ####################
+canvas = Canvas(Generator,
                 bg="#00B5C0",
                 height=600,
                 width=1000,
                 bd=0,
                 highlightthickness=0,
                 relief="ridge")
+
 canvas.place(x=0, y=0)
 
+global background
 background_img = PhotoImage(file=f"../assets/backgroundGenerator.png")
 background = canvas.create_image(500, 300, image=background_img)
 
+global generatedPassword_bg
 generatedPassword_img = PhotoImage(file=f"../assets/TextBox.png")
 generatedPassword_bg = canvas.create_image(180,
                                            380.0,
                                            image=generatedPassword_img)
-text = Text(root, bd=0, bg="#FFFFFF", fg="black", highlightthickness=0)
+
+text = Text(Generator, bd=0, bg="#FFFFFF", fg="black", highlightthickness=0)
 text.place(x=30, y=370, width=300, height=20)
+
+btnAdd = PhotoImage(file=f"../assets/buttonGenerator.png")
+
+btn2 = Button(Generator, text="Frame2", command=lambda: showFrame(manager))
+btn2.place(x=0, y=0)
+
+btnAdd = PhotoImage(file=f"../assets/buttonGenerator.png")
+btn = Button(Generator,
+             image=btnAdd,
+             bg="#00B5C0",
+             borderwidth=0,
+             highlightthickness=0,
+             command=lambda: GeneratePassword(),
+             relief="flat")
+btn.place(x=80, y=510)
 
 
 def GeneratePassword():
@@ -35,13 +68,47 @@ def GeneratePassword():
         text.insert(tk.END, f"{s}")
 
 
-btnAdd = PhotoImage(file=f"../assets/buttonGenerator.png")
-btn = Button(image=btnAdd,
-             bg="#00B5C0",
-             borderwidth=0,
-             highlightthickness=0,
-             command=lambda: GeneratePassword(),
-             relief="flat")
-btn.place(x=80, y=510, width=192, height=37)
+################## MANAGER ####################
+
+managerCanvas = Canvas(manager,
+                       bg="#00B5C0",
+                       height=600,
+                       width=1000,
+                       bd=0,
+                       highlightthickness=0,
+                       relief="ridge")
+managerCanvas.place(x=0, y=0)
+
+global manager_background
+managerbackground_img = PhotoImage(
+    file=f"../assets/manager/managerbackground.png")
+manager_background = managerCanvas.create_image(500,
+                                                300,
+                                                image=managerbackground_img)
+
+global passwordTextboxes
+background_password = PhotoImage(file=f"../assets/manager/Textboxes.png")
+passwordTextboxes = managerCanvas.create_image(500,
+                                               400,
+                                               image=background_password)
+
+password1 = Text(manager, bd=0, bg="#FFFFFF", fg="black", highlightthickness=0)
+password1.place(x=350, y=277, width=300, height=20)
+
+password2 = Text(manager, bd=0, bg="#FFFFFF", fg="black", highlightthickness=0)
+password2.place(x=350, y=333, width=300, height=20)
+
+password3 = Text(manager, bd=0, bg="#FFFFFF", fg="black", highlightthickness=0)
+password3.place(x=350, y=390, width=300, height=20)
+
+password4 = Text(manager, bd=0, bg="#FFFFFF", fg="black", highlightthickness=0)
+password4.place(x=350, y=450, width=300, height=20)
+
+password5 = Text(manager, bd=0, bg="#FFFFFF", fg="black", highlightthickness=0)
+password5.place(x=350, y=505, width=300, height=20)
+
+button = Button(manager, text="Back", command=lambda: showFrame(Generator))
+button.place(x=0, y=0)
+
 root.resizable(False, False)
 root.mainloop()
---
+++ README.md
@@ -0,0 +1,25 @@
+# Passnager in Python
+
+A version of my Passnager app made entirely in Python using Tkinter for the GUI.
+
+This is a project that I started for studying the Tkinter library.
+
+I'm working on adding the manager as soon as possible.
+
+Made with: @PedroKaram
+
+# Installation
+
+- Clone the Repository in your desired folder.
+
+- Run the MainPage.py file.
+
+- If you want to skip the login page, just run the GeneratorPage.py file.
+
+# Known Issues
+
+- This is an Alpha version. The login page is only for looks for now. The password generator is working as intended, the manager pages is added, but not connected to a database just yet. Any password saved in the manager will be lost after closing the program. For now.
+
+# Updates
+
+- Added manager page and a button to quick switch between the manager and the generator. Working on adding connection to a database and some cool animations.
